99e5bfb37e7fa1e1c3076e5138a007627df25868
Daniel Yuschick <yuschick@users.noreply.github.com>
2022-07-08 16:19:58 +0300

    feat: add border radius mixin to sc package (#35)
    Co-authored-by: Daniel Yuschick <daniel.yuschick@gmail.com>

---
+++ packages/styled-components/src/mixins/BorderRadius/BorderRadius.types.ts
@@ -0,0 +1,9 @@
+import { DefaultValue } from '../../types/global';
+
+export type BorderRadiusProps = {
+    bottomLeft?: DefaultValue;
+    bottomRight?: DefaultValue;
+    radius?: DefaultValue;
+    topLeft?: DefaultValue;
+    topRight?: DefaultValue;
+};
