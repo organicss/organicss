3f9db89ea90d90e294c88ceecfa08789132add84
Daniel Yuschick <yuschick@users.noreply.github.com>
2022-07-08 17:23:05 +0300

    feat: add layout mixin to sc package (#38)
    Co-authored-by: Daniel Yuschick <daniel.yuschick@gmail.com>

--- packages/styled-components/src/index.ts
+++ packages/styled-components/src/index.ts
@@ -1,5 +1,8 @@
 export * from './types/global';
 
+export * from './mixins/Border';
 export * from './mixins/BorderRadius';
+export * from './mixins/Layout';
 export * from './mixins/Margin';
 export * from './mixins/Padding';
+export * from './mixins/Position';
